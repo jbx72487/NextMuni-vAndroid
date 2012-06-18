fd50a72a0a701c7e697d17d4023057559999c313
Joy Xi <joy.b.xi@gmail.com>
2012-06-17 23:32:00 -0700

    Gave up on WebView b/c of geolocation reqs and just launched browser instead

--- NextMuniWebView/src/projects/jbx/NextMuniWebViewActivity.java
+++ NextMuniWebView/src/projects/jbx/NextMuniWebViewActivity.java
@@ -1,17 +1,20 @@
 package projects.jbx;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.KeyEvent;
+/* import android.view.KeyEvent;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import android.webkit.WebViewClient; */
+
 
 public class NextMuniWebViewActivity extends Activity {
-	WebView mWebView;
+	// WebView mWebView;
 	
     /** Called when the activity is first created. */
-    
 
+	/*
     private class NextMuniWebViewClient extends WebViewClient {
     	@Override
     	public boolean shouldOverrideUrlLoading(WebView view, String url) {
@@ -19,18 +22,32 @@ public class NextMuniWebViewActivity extends Activity {
     		return true;
     	}
     }
+    */
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
+        /*
         mWebView = (WebView) findViewById(R.id.webview);
         mWebView.getSettings().setJavaScriptEnabled(true);
-        mWebView.loadUrl("http://www.nextmuni.com");
+        mWebView.getSettings().setGeolocationEnabled(true);
+        mWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+*/
+        String url = "http://www.nextmuni.com";
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
+        startActivity(i);
+        
+/*        mWebView.loadUrl("http://www.nextmuni.com");
+        mWebView.loadUrl("http://www.nextmuni.com/#_routes")
+        
         mWebView.setWebViewClient(new NextMuniWebViewClient());
+        */
     }
     
+    /*
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
     	if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {
@@ -39,4 +56,8 @@ public class NextMuniWebViewActivity extends Activity {
     	}
     	return super.onKeyDown(keyCode, event);
     }
+    */
+    
+    
 }
+
